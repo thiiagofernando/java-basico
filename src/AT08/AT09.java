01e2679bca5103dbe8e0a5f1179beb68b9c0c008
Thiago Fernando <thiago.fernando@msn.com>
2017-05-01 18:43:25 -0400

    realizar a pergunta 5 vezes

---
+++ src/AT08/AT09.java
@@ -0,0 +1,41 @@
+package AT08;
+
+import java.util.Scanner;
+
+public class AT09 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			 double media,nota1,nota2,nota3,nota4;
+			    
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Informe a 1ª nota" );  
+		        nota1 = sc.nextDouble();
+		        
+		        System.out.println("Informe a 2ª nota" );  
+		        nota2 = sc.nextDouble();
+		        
+		        System.out.println("Informe a 3ª nota" );  
+		        nota3 = sc.nextDouble();
+		        
+		        System.out.println("Informe a 4ª nota" );  
+		        nota4 = sc.nextDouble();
+		        
+		        media = (nota1+nota2+nota3+nota4)/4;
+		        
+		        if(media >= 7)
+		        {
+		        	System.out.println("Aluno Aprovado com media: " + media);
+		        }if(media < 7)
+		        {
+		        	System.out.println("Aluno Reprovado com media: " + media);
+		        }
+		}
+		
+		System.out.println("FIM");
+
+	}
+
+}
