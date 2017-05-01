8f18fdaa8da9292c6f61a6009651bca263567644
Thiago Fernando <thiago.fernando@msn.com>
2017-05-01 17:10:23 -0400

    Calculadora

--- src/AT19/AT19.java
+++ src/AT19/AT19.java
@@ -5,75 +5,82 @@ import java.util.Scanner;
 public class AT19 {
 
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
+	
+		 	boolean continuar = true; 
+	        int opcao; 
+	        double num1,num2,soma,diminui,multiplica,divide;
+	        Scanner entrada = new Scanner(System.in); 
+	        	        
+	      try{
+	        while(continuar){ 
+	        	System.out.println("Entre com o 1º valor: ");
 
-		
-		System.out.print("Entre com o 1º valor: ");
+	        		num1 = entrada.nextInt();
 
-		int num1 = entrada.nextInt();
+	   		System.out.println("Entre com o 2º valor: ");
 
-		System.out.print("Entre com o 2º valor: ");
+	   				num2 = entrada.nextInt();
+	   				
+	   				
+	   				System.out.println("Escolha Operação: ");
+	   				System.out.println("1 Soma");
+	   				System.out.println("2 Subtração");
+	   				System.out.println("3 Multiplicação");
+	   				System.out.println("4 Divisão ");
+	   				System.out.println("0 Sair ");
+	   				System.out.println("Digite uma opcao: ");
+	   				opcao = entrada.nextInt(); 
+	   				
+	            
+	        
+	   			 if(opcao==0){ 
+	                continuar=false; 
+	                System.out.println("");
+	                System.out.println("Você fechou o programa"); 
+	            } 
+	            else{ 
+	                
+	            	if(opcao == 1)
+	            	{
+	            			soma = num1 + num2;
+							System.out.println("A Soma e: " + soma);
+							System.out.println("");
+	            	}
+	            	if(opcao == 2)
+	            	{
+	            			diminui = num1 - num2;
+							System.out.println("A Subtração e: " + diminui);
+							System.out.println("");
+	            	}
+	            	if(opcao == 3)
+	            	{
+	            		multiplica = num1 * num2;
+							System.out.println("A Multiplicação e: " + multiplica);
+							System.out.println("");
+	            	}
+	            	if(opcao == 4)
+	            	{
+	            		if(num1 < num2)
+	            		{
+	            			System.out.println("Não é possivel realizar o calculo" + " por que " + num1 + " é menor que " + num2 );
+	            			System.out.println("");
+	            		}else
+	            		{
+		            			divide = num1 / num2;
+								System.out.println("A Divisão e: " + divide);
+								System.out.println("");
+	            		}
+	            	}
+	            	else{
+	            		
+	            		System.out.println("Operação Invalida");
+	            	}
+	            }
+	        } 
+	        
+	      }catch(Exception e){
+	    	  System.out.println("Operação Invalida");
 
-		int num2 = entrada.nextInt();
-
-		System.out.println("SELECIONE UMA OPERACAO: ");
-		System.out.println("1 Soma");
-		System.out.println("2 Subtração");
-		System.out.println("3 Multiplicação");
-		System.out.println("4 Divisão ");
-		System.out.println("0 Sair ");
-		System.out.println("Digite uma opcao: ");
-
-		int opc = entrada.nextInt();
-
-	/*	do{
-			
-			
-		}while(opc != 0);
-		System.out.println("você saiu da calculadora");*/
-		
-		
-		switch (opc) {
-		case 1:
-
-			int soma = num1 + num2;
-			System.out.println("A soma e: " + soma);
-
-			break;
-
-		case 2:
-
-			int diminui = num1 - num2;
-			System.out.println("A subtracão e: " + diminui);
-
-			break;
-
-		case 3:
-
-			int multiplica = num1 * num2;
-
-			System.out.println("A multiplicacão e: " + multiplica);
-
-			break;
-
-		case 4:
-
-			if (num1 < num2) {
-				System.out.println("Impossivel realizar calculo!! \n");
-			} else {
-				int divide = num1 / num2;
-
-				System.out.println("A divisão e: " + divide);
-			}
-
-			break;
-
-		default:
-
-			System.out.print("Operacao invalida!!");
-
-		}
-
+	      }
+	    } 
 	}
-
-}
