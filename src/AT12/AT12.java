dc03d103748bed62f149bc43b5124921f7b1ad42
Thiago Fernando <thiago.fernando@msn.com>
2017-05-01 19:42:25 -0400

    AT 12 13

--- src/AT12/AT12.java
+++ src/AT12/AT12.java
@@ -1,9 +1,23 @@
 package AT12;
 
+import java.util.Scanner;
+
 public class AT12 {
 
 	public static void main(String[] args) {
 		
+		double raiz=0;  
+		do
+		{
+			 Scanner s = new Scanner (System.in);
+			   System.out.println("Informe um valor"); 
+			   raiz = s.nextDouble(); 
+			   System.out.println("A raiz quadrada de  " + raiz +"  é = " + Math.sqrt(raiz));
+			   
+		}
+		while(raiz > 0);
+			
+			  System.out.println("Fim Numero negativo");
 
 	}
 
